c736f55dc7f39396f4c2bab4ee1e74b5e3f7a1b6
mac <moyuanxinxi@163.com>
2019-11-15 23:05:03 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          后续功能等待开发中……
         </p>
         <a
           className="App-link"
