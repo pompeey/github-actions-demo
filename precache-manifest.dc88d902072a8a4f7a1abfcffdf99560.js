af48bdac98d5d3e764ed5f0a0bb2ccfc870ced16
pompeey <pompeey@163.com>
2019-11-15 15:31:46 +0000

    Deploying to gh-pages from master 8cb03306e1e81faf9c2eea4bec48c5b4f944de94

---
+++ precache-manifest.dc88d902072a8a4f7a1abfcffdf99560.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeea80d8f02a934a8d2917b88425d813",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "1f6adcf5932fa38dfb0c",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d2718bbd4f8fb500d63e",
+    "url": "/github-actions-demo/static/js/2.2f60c060.chunk.js"
+  },
+  {
+    "revision": "1f6adcf5932fa38dfb0c",
+    "url": "/github-actions-demo/static/js/main.adde74a8.chunk.js"
+  },
+  {
+    "revision": "dfb417f264dd525bbb87",
+    "url": "/github-actions-demo/static/js/runtime~main.553754d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
